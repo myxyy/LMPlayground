d17c06f858d1a5c9c3e8d98cb493649f7c8fb931
myxyy <m.myxomatosis@gmail.com>
2025-06-21 12:11:35 +0000

    remove comments

--- src/ml_playground/predict_stream.py
+++ src/ml_playground/predict_stream.py
@@ -17,9 +17,7 @@ model.cuda()
 text_prefix = "仙台市は、"
 tokenized_prefix = tokenizer(text_prefix, return_tensors="pt")["input_ids"]
 
-#id_list = tokenized_prefix[0].tolist()
-
-streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+streamer = TextStreamer(tokenizer, skip_prompt=False, skip_special_tokens=True)
 
 x = tokenized_prefix.cuda() 
 hidden = model.hidden_init[None,:,:]
@@ -28,18 +26,11 @@ streamer.put(tokenized_prefix)
 
 max_length = 1024
 for i in range(max_length):
-    #print(f"Step {i+1}/{max_length}")
     with torch.no_grad():
         y, hidden = model.forward_with_hidden(x, hidden)
     y = y[0, -1, :]
-    #id_next = torch.argmax(y, dim=-1)
     id_next = torch.multinomial(torch.softmax(y, dim=-1), num_samples=1)
     streamer.put(id_next)
-    #print(id_next)
-    #print(id_next.item())
-    #print(id_next.shape)
-    #id_list.append(id_next.item())
     x = id_next[None].cuda()
 
-#text = tokenizer.decode(id_list, skip_special_tokens=True)
-#print(text)
+streamer.end()
