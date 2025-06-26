8b70d527703b83ac993071a8db7028ec6d4b7b7d
myxyy <m.myxomatosis@gmail.com>
2025-06-26 12:58:36 +0000

    schedulefree組み込み

--- src/lm_playground/train_qlstm.py
+++ src/lm_playground/train_qlstm.py
@@ -2,10 +2,12 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 from lm_playground.model.qlstm import QLSTMModel, QLSTMConfig
 from lm_playground.trainer import Trainer
+import torch
 
 if __name__ == "__main__":
     dataset = load_dataset("graelo/wikipedia", "20230601.ja", split="train", cache_dir="resources/datasets")
     dataset = dataset["text"]
+    train_dataset, validation_dataset = torch.utils.data.random_split(dataset, [len(dataset) - 1000, 1000], generator=torch.Generator().manual_seed(42))
     #dataset = load_dataset("globis-university/aozorabunko-clean", split="train", cache_dir="resources/datasets")
     #dataset = dataset["text"]
     #dataset = dataset.take(100000)
@@ -27,13 +29,15 @@ if __name__ == "__main__":
     trainer = Trainer(
         model=model,
         tokenizer=tokenizer,
-        dataset=dataset,
+        train_dataset=train_dataset,
+        validation_dataset=validation_dataset,
         batch_size=6,
         max_length=1024,
         max_epochs=1,
         model_name="qlstm",
         checkpoint_path="resources/checkpoints/qlstm",
-        checkpoint_interval=5000
+        checkpoint_interval=5000,
+        validation_interval=500
     )
 
     trainer.train()
--- src/lm_playground/trainer.py
+++ src/lm_playground/trainer.py
@@ -8,28 +8,32 @@ from torch.distributed import init_process_group, destroy_process_group
 import os
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch
-from transformers import DataCollatorWithPadding
+from schedulefree import RAdamScheduleFree
+from torch.utils.data import DataLoader
 
 class Trainer:
     def __init__(
             self,
             model,
             tokenizer,
-            dataset,
+            train_dataset,
+            validation_dataset,
             model_name,
             batch_size=1,
             max_length=4096,
             max_epochs=1,
             checkpoint_path=None,
-            checkpoint_interval=1000
+            checkpoint_interval=1000,
+            validation_interval=1000
         ):
         self.gpu_id = int(os.environ["LOCAL_RANK"])
         self.model = model
         self.tokenizer = tokenizer
-        self.dataset = dataset
+        self.train_dataset = train_dataset
+        self.validation_dataset = validation_dataset
         self.batch_size = batch_size
         self.max_length = max_length
-        self.optimizer = AdamW(self.model.parameters(), lr=1e-4, weight_decay=0.01)
+        self.optimizer = RAdamScheduleFree(self.model.parameters(), lr=1e-4, betas=(0.9, 0.999))
         self.max_epochs = max_epochs
         self.criterion = nn.CrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
         self.model_name = model_name
@@ -37,6 +41,7 @@ class Trainer:
         self.current_step = 0
         self.checkpoint_interval = checkpoint_interval
         self.current_epoch = 0
+        self.validation_interval = validation_interval
     
     def save_checkpoint(self):
         if self.gpu_id == 0:
@@ -46,7 +51,7 @@ class Trainer:
             torch.save({
                 'model_state_dict': self.model.module.state_dict(),
                 'optimizer_state_dict': self.optimizer.state_dict(),
-                'dataloader_state_dict': self.dataloader.state_dict() if hasattr(self, 'dataloader') else None,
+                'train_dataloader_state_dict': self.train_dataloader.state_dict() if hasattr(self, 'train_dataloader') else None,
                 'epoch': self.current_epoch,
                 'step': self.current_step
             }, checkpoint_file)
@@ -63,7 +68,7 @@ class Trainer:
                 checkpoint = torch.load(checkpoint_file, map_location='cpu')
                 self.model.module.load_state_dict(checkpoint['model_state_dict'])
                 self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-                self.dataloader.load_state_dict(checkpoint['dataloader_state_dict'])
+                self.train_dataloader.load_state_dict(checkpoint['train_dataloader_state_dict'])
                 self.current_epoch = checkpoint['epoch']
                 self.current_step = checkpoint['step']
                 print(f"Loaded checkpoint from {checkpoint_file}")
@@ -74,19 +79,18 @@ class Trainer:
 
         self.model = DDP(self.model.cuda(), device_ids=[self.gpu_id])
         collator = lambda t: self.tokenizer(t, truncation=True, padding="max_length", max_length=self.max_length+1, return_tensors="pt")
-        self.dataloader = StatefulDataLoader(self.dataset, collate_fn=collator, batch_size=self.batch_size, pin_memory=True, sampler=DistributedSampler(self.dataset), num_workers=4)
+        self.train_dataloader = StatefulDataLoader(self.train_dataset, collate_fn=collator, batch_size=self.batch_size, pin_memory=True, sampler=DistributedSampler(self.train_dataset), num_workers=4)
+        self.validation_dataloader = DataLoader(self.validation_dataset, collate_fn=collator, batch_size=self.batch_size, pin_memory=True, sampler=DistributedSampler(self.validation_dataset), num_workers=4)
 
         self.load_checkpoint()
 
         while self.current_epoch < self.max_epochs:
-            pbar = tqdm(self.dataloader, initial=self.current_step, total=len(self.dataloader))
-            for i, batch in enumerate(pbar):
+            pbar = tqdm(self.train_dataloader, initial=self.current_step, total=len(self.train_dataloader))
+            for _, batch in enumerate(pbar):
+                self.model.train()
+                self.optimizer.train()
                 self.optimizer.zero_grad()
-                inputs = batch["input_ids"][:, :-1].cuda()
-                targets = batch["input_ids"][:, 1:].cuda()
-                #print(inputs)
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs.view(-1, self.tokenizer.vocab_size), targets.view(-1))
+                loss = self.calculate_loss(batch)
                 loss.backward()
                 self.optimizer.step()
                 pbar.set_postfix({
@@ -94,6 +98,16 @@ class Trainer:
                     "lr": self.optimizer.param_groups[0]["lr"]
                 })
                 self.current_step += 1
+                if self.current_step % self.validation_interval == 0:
+                    self.model.eval()
+                    self.optimizer.eval()
+                    pbar_validation = tqdm(self.validation_dataloader, initial=self.current_step, total=len(self.validation_dataloader))
+                    for _, val_batch in enumerate(pbar_validation):
+                        with torch.no_grad():
+                            val_loss = self.calculate_loss(val_batch)
+                            pbar_validation.set_postfix({
+                                "val_loss": val_loss.item()
+                            })
                 if self.current_step % self.checkpoint_interval == 0:
                     self.save_checkpoint()
             self.current_epoch += 1
@@ -106,3 +120,10 @@ class Trainer:
             torch.save(self.model.module.cpu().state_dict(), weight_file)
     
         destroy_process_group()
+    
+    def calculate_loss(self, batch):
+        inputs = batch["input_ids"][:, :-1].cuda()
+        targets = batch["input_ids"][:, 1:].cuda()
+        outputs = self.model(inputs)
+        loss = self.criterion(outputs.view(-1, self.tokenizer.vocab_size), targets.view(-1))
+        return loss
