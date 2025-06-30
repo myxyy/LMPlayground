156600827c5912cec4ce0ee960ec9b828393c130
myxyy <m.myxomatosis@gmail.com>
2025-06-30 12:15:32 +0000

    入力できるようにする

--- src/lm_playground/predict_stream.py
+++ src/lm_playground/predict_stream.py
@@ -17,24 +17,30 @@ model.load_state_dict(torch.load("resources/checkpoints/qlstm/qlstm.pth", map_lo
 model.eval()
 model.cuda()
 
-text_prefix = "仙台市は、"
-tokenized_prefix = tokenizer(text_prefix, return_tensors="pt")["input_ids"]
+def predict(text_prefix):
+    tokenized_prefix = tokenizer(text_prefix, return_tensors="pt")["input_ids"]
 
-streamer = TextStreamer(tokenizer, skip_prompt=False, skip_special_tokens=True)
+    streamer = TextStreamer(tokenizer, skip_prompt=False, skip_special_tokens=True)
 
-x = tokenized_prefix.cuda() 
-hidden = model.hidden_init[None,:,:]
+    x = tokenized_prefix.cuda() 
+    hidden = model.hidden_init[None,:,:]
 
-streamer.put(tokenized_prefix)
+    streamer.put(tokenized_prefix)
 
-max_length = 1024
-temperature = 0.9
-for i in range(max_length):
-    with torch.no_grad():
-        y, hidden = model.forward_with_hidden(x, hidden)
-    y = y[0, -1, :]
-    id_next = torch.multinomial(torch.softmax(y / temperature, dim=-1), num_samples=1)
-    streamer.put(id_next)
-    x = id_next[None].cuda()
+    max_length = 1024
+    temperature = 1.0
+    for i in range(max_length):
+        with torch.no_grad():
+            y, hidden = model.forward_with_hidden(x, hidden)
+        y = y[0, -1, :]
+        id_next = torch.multinomial(torch.softmax(y / temperature, dim=-1), num_samples=1)
+        streamer.put(id_next)
+        x = id_next[None].cuda()
 
-streamer.end()
+    streamer.end()
+
+while True:
+    text_prefix = input("Enter text prefix: ")
+    if text_prefix.lower() == "exit":
+        break
+    predict(text_prefix)
